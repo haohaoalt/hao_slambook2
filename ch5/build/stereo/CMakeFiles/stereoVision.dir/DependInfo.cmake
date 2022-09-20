76e58b499df57ee2b18c587774e4e498af7191dc
hao007 <haohaoalt@163.com>
2022-09-20 12:56:42 +0800

    add ch5

---
+++ ch5/build/stereo/CMakeFiles/stereoVision.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hao007/github/hao_slambook2/ch5/stereo/stereoVision.cpp" "/home/hao007/github/hao_slambook2/ch5/build/stereo/CMakeFiles/stereoVision.dir/stereoVision.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/home/hao007/haolib/opencv4.6/install/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ch5/hw5.md
@@ -0,0 +1,15 @@
+<!--
+ * @Author: zhanghao
+ * @Date: 2022-09-20 12:45:57
+ * @LastEditTime: 2022-09-20 12:54:55
+ * @FilePath: /hao_slambook2/ch5/hw5.md
+ * @Description: 
+-->
+# HW5
+## 01 寻找一部相机标定他的内参
+## 02 叙述相机内参的物理意义。如果一部相机的分辨率变为原来的两倍而其他地方不变，那么他的内参如何变化？
+## 03 搜索特殊相机的标定方法。他们与普通针孔有和不同？
+## 04 调研全局快门相机（global shutter)相机和卷帘快门（rolling shutter）相机的异同。他们在SLAM中有和缺点？
+## 05 RGB-D相机如何标定？以kinect为例 需要标定哪些参数？
+https://github.com/code-iai/iai_kinect2)
+## 06 除了示例遍历图像的方法，列举其他遍历图像的方法。
